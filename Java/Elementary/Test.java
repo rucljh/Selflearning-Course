e585832882d2e4185ec1996a63ad9f8abb119fa6
Local <45068126+rucljh@users.noreply.github.com>
2020-10-06 19:46:12 +0800

    day 4

---
+++ Java/Elementary/Test.java
@@ -0,0 +1,104 @@
+/**
+* @Description 
+* @Author L.John
+* @Version 1.0.0
+* @Date 2020-10-04 13:56:56
+*/
+
+public class Test{
+    public static void main(String[] args){
+        // // 1.Hello World!
+        // System.out.println("Hello World!");
+        // // 2.Value types
+        // char c = 'a';//单引号只用于单个字符
+        // System.out.println(c);
+        // // 3.Reference types
+        // String str = "Hello" + " World!";
+        // System.out.println(str);//双引号可用于多个字符
+        // // 4.operation
+        // System.out.println(7 / 2.0);
+        // int i1 = 10, i2 = 20;
+        // int i = i1++;//先对i赋值，i1再+1
+        // System.out.println(+i);
+        // System.out.println(+i1);
+        // i = ++i1;//先对i1+1，再对i赋值
+        // System.out.println(+i);
+        // System.out.println(+i1);
+        // i = i2--;
+        // System.out.println(+i);
+        // System.out.println(+i2);
+        // i = --i2;
+        // System.out.println(+i);
+        // System.out.println(+i2);
+        // // 5.assignment operation
+        // short s = 5;
+        // s += 3;//+=可用于隐式强制变换数据类型
+        // s = (short) (s + 1);
+        // System.out.println(s);
+        // // 6.Bitwise operation
+        // short s = -128;
+        // System.out.println(s>>1);//二进制变量值左边第一位为符号位，用于判断值位数右移后左边补0还是1
+        // // 7.Operation rank
+        // System.out.println(1 + 6 % 4 * 2 + 1);//相同优先级从代码左侧往右顺序运算；不同优先级，优先级高的先运算
+        // // 8.Structure
+        // int i = 3;
+        // if (i == 1){
+        //     System.out.println("1");
+        // }else if (i == 2){
+        //     System.out.println("2");
+        // }else {
+        //     System.out.println("3");
+        // }
+
+        // int i = 1;
+        // switch (i){
+        //     case 1:
+        //         System.out.println("a");
+        //         break;
+        //     case 2:
+        //         System.out.println("b");
+        //         break;
+        //     default:
+        //         System.out.println("0");
+        //         break;
+        // // 9.Loop structure
+        // for (int i = 0; i < 5; i++){
+        //     System.out.println("Hello World!");
+        // }
+
+        // int i = 1;
+        // while (i <= 5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int m = 1;
+        // do{
+        //     System.out.println(m);
+        //     m++;
+        // }while(m <= 5);
+
+        // for (int i = 0; i < 2; i++){//建议外层循环次数小于内层循环次数
+        //     for (int j = 0; j < 5; j++){
+
+        //     }
+        // }
+
+        // for (int i = 2; i <= 100; i++){// 找出2-100内质数
+        //     int k = 0;
+        //     for (int j = 2; j < i; j++){
+        //         if (i % j == 0){
+        //             k++;
+        //         }
+        //     }
+        //     if (k == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+        // // 10.Special structure
+        // break终止当前所在循环；continue跳过一次当前所在循环，不执行该次循环内的后面代码；return结束整个方法
+        // // 11.
+        
+    }
+}
+

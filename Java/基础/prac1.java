47ac13e9f27bd94360b874194a74da16b8bab488
Local <45068126+rucljh@users.noreply.github.com>
2020-10-07 15:14:23 +0800

    2021.4.15 update

---
+++ Java/基础/prac1.java
@@ -0,0 +1,12 @@
+/**
+* @Description Intermediate practise of java
+* @Author L.John
+* @Version 1.0.0
+* @Date 2020-10-07 15:15:21
+*/
+
+public class prac1{
+    public static void main(String[] args){
+        System.out.println(1);
+    }
+}
